6dfe1fc8384f47bad6aeaf9fb201af73b277b299
kaushik raj <algorithmunloack@gmail.com>
2024-03-24 14:48:07 +0530

    Basic code has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main (){
-  runApp(const Kauhik());
+  runApp(const AZNET ());
 }
+class AZNET extends StatelessWidget {
+const AZNET ({super.key});
+   @override
 
-class Kauhik extends StatelessWidget {
-  const Kauhik ({super.key});
-  @override
-
-Widget build (BuildContext context){
-  return const Text(
-    'hello ',textDirection: TextDirection.ltr,
-  );
-}
+   Widget build (BuildContext context){
+     return MaterialApp(
+      home:Scaffold (
+        body: Center (child:Text ('Hi KAUSHIK')
+        ),
+      ),
+     );
+   }
 }
